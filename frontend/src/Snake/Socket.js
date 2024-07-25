7cabc0038c6bd0a5a07415396dc011fbadfba033
7-Dany <ali.ali.ameen245@gmail.com>
2024-07-25 13:09:21 +0300

    change location

---
+++ frontend/src/Snake/Socket.js
@@ -0,0 +1,39 @@
+import { io } from "socket.io-client";
+import EventEmitter from './misc/EventEmitter';
+
+class Socket extends EventEmitter {
+    constructor(url) {
+        super();
+        this.username = "";
+
+        this.socket = io(url);
+
+        this.socket.on('connect', this.connect);
+    }
+
+    emitEvent = (event) => {
+        this.socket.on(event, (...args) => {
+            this.emit(event, ...args);
+        });
+    }
+
+    connect = () => {
+        this.emitEvent("receive_username");
+        this.emitEvent("opponent_info");
+        this.emitEvent("opponent_ready");
+    }
+
+    registerUsername = (username) => {
+        this.socket.emit("send_username", username);
+    }
+
+    startMatching = () => {
+        this.socket.emit("start_matching", this.username);
+    }
+
+    readyForStart = () => {
+        this.socket.emit("ready_for_start");
+    }
+}
+
+export default Socket;
